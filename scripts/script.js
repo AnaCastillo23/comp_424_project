bce9d68142cf95bbeb160a1cde45239146dd2c29
AnaCastillo23 <89889778+AnaCastillo23@users.noreply.github.com>
2024-03-26 11:44:57 -0700

    Password strenght checker 1.2 (works noew)

--- scripts/script.js
+++ scripts/script.js
@@ -1,43 +1,41 @@
-const passwordInput = document.querySelector("input"),
-indicator = document.querySelector(".indicator"),
-iconText = document.querySelector(".icon-text"),
-text = document.querySelector(".text");
+const indicator = document.querySelector('.indicator'),
+iconText = document.querySelector('.icon-text'),
+text = document.querySelector('.text');
 
 
-function Strength(passwordInput) {
+function Strength(password) {
     let i = 0;
-    if(passwordInput.length > 6) {
+    if(password.length > 6) {
         i++;
     }
-    if(passwordInput.length >= 10) {
-        i++
+    if(password.length >= 10) {
+        i++;
     }
-    if(/[A-Z]/.test(passwordInput)) {
-        i++
+    if(/[A-Z]/.test(password)) {
+        i++;
     }
-    if(/[a-z]/.test(passwordInput)) {
-        i++
+    if(/[a-z]/.test(password)) {
+        i++;
     }
-    if(/[0-9]/.test(passwordInput)) {
-        i++
+    if(/[0-9]/.test(password)) {
+        i++;
     }
-    if(/[A-Za-z0-9]/.test(passwordInput)) {
-        i++
+    if(/[A-Za-z0-8]/.test(password)) {
+        i++;
     }
     return i;
-
 }
 
 let wrapper = document.querySelector('.wrapper');
-document.addEventListener('keyup', function(e){
-    let passwordInput = document.querySelector('input').value;
+document.addEventListener("keyup",function(e){
+    let passwordInput = document.querySelector('#myPassword').value;
 
     let strength = Strength(passwordInput);
-    if(strength <=2) {
+    if(strength <= 2) {
         wrapper.classList.add('weak');
         wrapper.classList.remove('medium');
         wrapper.classList.remove('strong');
-    } else if (strength >= 2 || strength <= 4) {
+    } else if (strength >= 2 && strength <= 4) {
         wrapper.classList.remove('weak');
         wrapper.classList.add('medium');
         wrapper.classList.remove('strong');
@@ -47,3 +45,6 @@ document.addEventListener('keyup', function(e){
         wrapper.classList.add('strong');
     }
 })
+
+
+
