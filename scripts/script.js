6554d755462c40c384648167c8c9ddbb898aa59f
AnaCastillo23 <89889778+AnaCastillo23@users.noreply.github.com>
2024-03-25 18:33:04 -0700

    Working on password strength checker

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,41 @@
-const signInBtnLink =  document.querySelector('.sign-in-link');
-const signUpBtnLink =  document.querySelector('.sign-up-link');
-const wrapper =  document.querySelector('.wrapper');
+const passwordInput = document.querySelector("input"),
+indicator = document.querySelector(".indicator"),
+iconText = document.querySelector(".icon-text"),
+text = document.querySelector(".text");
 
-signUpBtnLink.addEventListener('click', () => {
-    wrapper.classList.toggle('active');
+
+function Strength(passwordInput) {
+    let i = 0;
+    if(passwordInput.length > 6) {
+        i++;
+    }
+    if(passwordInput.length >= 10) {
+        i++
+    }
+    if(/[A-Z]/.test(passwordInput)) {
+        i++
+    }
+    if(/[a-z]/.test(passwordInput)) {
+        i++
+    }
+    if(/[0-9]/.test(passwordInput)) {
+        i++
+    }
+    if(/[A-Za-z0-9]/.test(passwordInput)) {
+        i++
+    }
+    return i;
+
+}
+
+let wrapper = document.querySelector('.wrapper');
+document.addEventListener("keyup", function(e){
+    let passwordInput = document.querySelector("input").value;
+
+    let strength = Strength(passwordInput);
+    if(strength <=2) {
+        wrapper.classList.add('weak');
+        wrapper.classList.remove('medium');
+        wrapper.classList.remove('strong');
+    }
 })
