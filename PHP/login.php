23a0b8f9605468172a6af7cdd3e5b8f9051e1787
Andrew <andrew.akhoian.775@my.csun.edu>
2024-04-13 15:02:18 -0700

    Added login counter. Also, tracks when a user logins in by updating user_login_t.

--- PHP/login.php
+++ PHP/login.php
@@ -12,6 +12,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $user = $result->fetch_assoc();
 
     if ($user && password_verify($password, $user['secret'])) {
+        //Increases login counter upon successful login
+        $update_login_count = $con->prepare("UPDATE user_t SET num_of_logins = COALESCE(num_of_logins, 0) + 1 WHERE username = ?");
+        $update_login_count->bind_param("s", $username);
+        $update_login_count->execute();
+        $update_login_count->close();
+
+        $user_id = $user['userID'];
+        //Tracks when the user logged in. Updates user_login_t
+        $login_time = date('Y-m-d H:i:s');
+        $insert_login_time = $con->prepare("INSERT INTO user_login_t(userID, login_time) VALUES(?, ?)");
+        $insert_login_time->bind_param("is", $user_id, $login_time);
+        $insert_login_time->execute();
+        $insert_login_time->close();
+
         $_SESSION['username'] = $username;
         header("Location: /comp_424_project/html/home-page.html");
         exit;
